bb277d6b9782a33fbb17455a187c8e7f907437ee
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-01-22 23:16:53 +0900

    メインコンテンツの横幅を整える

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function About() {
-  return <Hero title="About" subtitle="About development activities" />
+  return (
+    <Container>
+      <Hero title="About" subtitle="About development activities" />
+    </Container>
+  )
 }
