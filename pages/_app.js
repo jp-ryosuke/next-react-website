bc0db11b6a8e18e4efa8548ee68054947fc5a0b0
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-01-27 20:32:46 +0900

    Next.js 用の設定を追加する p171

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
+//Font Awesome の設定
+//Font Awesome の css をインポート
+//グローバルスタイルとしてサイト全体に適用する
+import '@fortawesome/fontawesome-svg--core/styles.css'
+
+//Font Awesome の svg コアが個別に css を適用するのを無効化
+import { config } from '[fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
