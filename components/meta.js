a92d85242330968427c454eb9cdb4fff1447dc09
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-01-29 00:23:33 +0900

    ページタイトルが未設定な場合の処理を指定する p193

--- components/meta.js
+++ components/meta.js
@@ -6,12 +6,12 @@ const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
   siteMeta
 
 export default function Meta({ pageTitle }) {
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+
   return (
     <Head>
-      <title>
-        {pageTitle} | {siteTitle}
-      </title>
-      <meta property="og:title" content={`${pageTitle} | ${siteTitle}`} />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
     </Head>
   )
 }
