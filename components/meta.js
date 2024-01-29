a96f1119b7d76ead936783388561cd7c0bc53368
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-01-29 23:58:56 +0900

    ページタイトル以外のメタデータを追加する p195

--- components/meta.js
+++ components/meta.js
@@ -5,13 +5,19 @@ import { siteMeta } from 'lib/constants'
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
   siteMeta
 
-export default function Meta({ pageTitle }) {
+export default function Meta({ pageTitle, pageDesc }) {
+  //ページタイトル
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+  //ページの説明
+  const desc = pageDesc ?? siteDesc
 
   return (
     <Head>
       <title>{title}</title>
       <meta property="og:title" content={title} />
+
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
     </Head>
   )
 }
