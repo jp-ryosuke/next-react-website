94cdb98f6741c59be9f431191a4855d3cd004297
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-01-30 22:54:20 +0900

    OGP 画像を追加する p198

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,13 @@ import eyecatch from 'images/about.jpg'
 export default function About() {
   return (
     <Container>
-      <Meta pageTitle="アバウト" pageDesc="About development activities" />
+      <Meta
+        pageTitle="アバウト"
+        pageDesc="About development activities"
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        apgeImgH={eyecatch.height}
+      />
       <Hero title="About" subtitle="About development activities" />
 
       <figure>
