80d089b4b083f2e016318ee1b0107ec1f76cf89a
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-05 00:09:30 +0900

    ESLint が通らなくなってたので .eslintrc.js に変更した next/babel も extends で読み込まないようにした

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'prettier'],
+  ignorePatterns: ['node_modules', 'dist'],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+}
