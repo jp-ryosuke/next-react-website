260a50e4505b01bbef2ab91ac0530431eb6c21c9
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-11 01:41:29 +0900

    microCMS から取得した本文のデータは html の文字列なので React 要素に変換する p235

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,12 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from 'components/two-column'
 import Image from 'next/legacy/image'
 
 export default function Schedule({
@@ -25,6 +31,14 @@ export default function Schedule({
             priority
           />
         </figure>
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <div dangerouslySetInnerHTML={{ __html: content }} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar></TwoColumnSidebar>
+        </TwoColumn>
       </article>
     </Container>
   )
