9ebd0c069c428cc760eda71fd6eeb8ecf873cc42
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-11 01:19:23 +0900

    ConvertDate コンポーネントをインポートする

--- components/post-header.js
+++ components/post-header.js
@@ -1,6 +1,7 @@
 import styles from 'styles/post-header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-regular-svg-icons'
+import ConvertDate from 'components/convert-date'
 
 export default function PostHeader({ title, subtitle, publish = '' }) {
   return (
@@ -10,7 +11,7 @@ export default function PostHeader({ title, subtitle, publish = '' }) {
       {publish && (
         <div className={styles.publish}>
           <FontAwesomeIcon icon={faClock} size="lg" color="var(--gray-25)" />
-          {publish}
+          <ConvertDate dateISO={publish} />
         </div>
       )}
     </div>
