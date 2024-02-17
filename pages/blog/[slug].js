69dca3b32a0aa14f0b5de0b967417b73bbe1647b
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-18 00:48:04 +0900

    getStaticPaths を用意する p253

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -64,13 +64,20 @@ export default function Post({
   )
 }
 
+export async function getStaticPaths() {
+  return {
+    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    fallback: false,
+  }
+}
+
 /* async/await
 async をつけて関数を宣言すると非同期関数を定義でき、特別な処理を入れなくても Promise オブジェクトになる
 非同期関数では await 式が使えて await 式は Promise オブジェクトの状態を評価してその状態が Fulfilled か Rejected に変わるまで待つ
 Fulfilled か Rejected に変わると次の処理へと進む*/
 
-export async function getStaticProps() {
-  const slug = 'micro'
+export async function getStaticProps(context) {
+  const slug = context.params.slug
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
