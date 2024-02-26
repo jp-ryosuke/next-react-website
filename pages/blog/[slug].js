7b13b1d990f53e97dc544fa07d894435bf94b133
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-26 23:39:24 +0900

    ページネーションのコンポーネントでリンクを表示する p262

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -12,6 +12,7 @@ import {
 } from 'components/two-column'
 import ConvertBody from '/components/convert-body'
 import PostCategories from 'components/post-categories'
+import Pagination from 'components/pagination'
 import Image from 'next/legacy/image'
 import { getPlaiceholder } from 'plaiceholder'
 
@@ -62,12 +63,12 @@ export default function Post({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
-        <div>
-          {prevPost.title} {prevPost.slug}
-        </div>
-        <div>
-          {nextPost.title} {nextPost.slug}
-        </div>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   )
