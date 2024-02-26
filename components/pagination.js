20db4e1abe3b89022d2e3228e5b408ba84d72035
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-26 23:24:27 +0900

    ページネーションのコンポーネントを作成する

---
+++ components/pagination.js
@@ -0,0 +1,7 @@
+import styles from 'styles/pagination.module.css'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons'
