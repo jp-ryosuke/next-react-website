5329da86e45849c89a42a26c32fb764b2dc8b16d
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-02-28 23:53:27 +0900

    getAllPosts ですべての記事データを取得する p269

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,23 @@
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import { getAllPosts } from 'lib/api'
 
-export default function Blog() {
+export default function Blog({ posts }) {
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
       <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
